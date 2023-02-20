78e520d6f125e46d085a746582701af9525208d7
Antoine Mariani <antoine.mariani@me.com>
2023-02-20 17:42:44 +0100

    Timer, reset, pause, resume = done. Committing before time setting functionnality

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-// import { setupCounter } from "./counter.js";
-import { setupCounter } from "./pomodoro-app.js";
+/* eslint-disable object-curly-spacing */
+import './style.css';
+// import javascriptLogo from './javascript.svg';
+// import { setupCounter } from './counter.js';
+import { setupCounter } from './pomodoro-app.js';
 
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a href='https://vitejs.dev' target='_blank'>
+      <img src='/vite.svg' class='logo' alt='Vite logo' />
     </a>
     <h1>Hello Pomodoro!</h1>
-    <div id="counter">
-      <span id="minutes">25</span>
-      <span id="colon">:</span>
-      <span id="seconds">00</span>
-      <div class="button" id="start">Start</div>
-      <div id="pause"></div>
+    <div id='counter'>
+      <span id='minutes'>25</span>
+      <span id='colon'>:</span>
+      <span id='seconds'>00</span>
+      <div id='btn-box'>
+        <div id='start'></div>
+        <div id='pause'></div>
+        <div class='button' id='reset'>Reset</div>
+      </div>
+      <div id='settings'>
+        <div id='focus'>
+          <p class='label'>Focus</p>
+          <button class='button' id='plus-focus'>+</button>
+          <span id='set-focus'>25</span>
+          <p class='label'>min.</p>
+          <button class='button' id='minus-focus'>-</button>
+        </div>
+        <div id='break'>
+          <p class='label'>Break</p>
+          <button class='button' id='plus-break'>+</button>
+          <span id='set-break'>5</span>
+          <p class='label'>min.</p>
+          <button class='button' id='minus-break'>-</button>
+        </div>
+      </div>
     </div>
   </div>
 `;
 
-setupCounter(document.querySelector("#counter"));
+setupCounter(document.querySelector('#counter'));
--- pomodoro-app.js
+++ pomodoro-app.js
@@ -1,61 +1,83 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable require-jsdoc */
 export function setupCounter(element) {
-  // write a function that takes a counter html element and starts a pomodoro timer
-  let startButton = element.querySelector("#start");
-  // when the counter is clicked
-  startButton.addEventListener("click", (event) => {
-    event.currentTarget.remove();
+  const startButton = element.querySelector('#start');
+  startButton.classList.add('button');
+  startButton.innerText = 'Start';
+
+  // Counter starter
+  const startCounter = (el) => {
+    const minutesDisplay = el.querySelector('#minutes');
+    const secondsDisplay = el.querySelector('#seconds');
+    const pauseButton = el.querySelector('#pause');
+    let minutes = 24;
+    let seconds = 12;
+    let isPaused = false;
+
+    // Timer function - setup
+    const timerOn = setInterval(() => {
+      if (seconds === 0) {
+        minutes -= 1;
+        seconds = 60;
+      }
+      seconds -= 1;
+      if (minutes === 0 && seconds === 0) {
+        clearInterval(timerOn);
+      }
+      minutesDisplay.innerText = `${minutes < 10 ? '0' : ''}${minutes}`;
+      secondsDisplay.innerText = `${seconds < 10 ? '0' : ''}${seconds}`;
+    }, 1000);
+
+    // Timer launcher
+    let timer = timerOn;
+
+    // Pause Button - setup
+    pauseButton.classList.add('button');
+    pauseButton.innerText = 'Pause';
+
+    // Pause Button - trigger
+    pauseButton.addEventListener('click', () => {
+      isPaused = !isPaused;
+      if (isPaused) {
+        pauseButton.innerText = 'Resume';
+        clearInterval(timer);
+      } else {
+        pauseButton.innerText = 'Pause';
+        timer = setInterval(() => {
+          if (seconds === 0) {
+            minutes -= 1;
+            seconds = 60;
+          }
+          seconds -= 1;
+          if (minutes === 0 && seconds === 0) {
+            clearInterval(timer);
+          }
+          minutesDisplay.innerText = `${minutes < 10 ? '0' : ''}${minutes}`;
+          secondsDisplay.innerText = `${seconds < 10 ? '0' : ''}${seconds}`;
+        }, 1000);
+      }
+    });
+
+    // Reset Button
+    const resetButton = element.querySelector('#reset');
+    resetButton.addEventListener('click', () => {
+      clearInterval(timer);
+      minutes = 25;
+      seconds = 0;
+      minutesDisplay.innerText = `${minutes < 10 ? '0' : ''}${minutes}`;
+      secondsDisplay.innerText = `${seconds < 10 ? '0' : ''}${seconds}`;
+      pauseButton.classList.remove('button');
+      pauseButton.innerText = '';
+      startButton.classList.add('button');
+      startButton.innerText = 'Start';
+    });
+  };
+
+  // Counter initializer
+  startButton.addEventListener('click', (event) => {
+    event.currentTarget.classList.toggle('button');
+    // eslint-disable-next-line no-param-reassign
+    event.currentTarget.innerText = '';
     startCounter(element);
   });
-  // the timer should count down from 25 minutes
-  // when the timer reaches 0, the counter should be reset to 25 minutes
-  // the counter should display the time in minutes and seconds
 }
-
-const startCounter = (element) => {
-  let minutesDisplay = element.querySelector("#minutes");
-  let secondsDisplay = element.querySelector("#seconds");
-  let pauseButton = element.querySelector("#pause");
-  let minutes = 25;
-  let seconds = 0;
-  let isPaused = false;
-
-  let timer = setInterval(() => {
-    if (seconds === 0) {
-      minutes -= 1;
-      seconds = 60;
-    }
-    seconds -= 1;
-    if (minutes === 0 && seconds === 0) {
-      clearInterval(timer);
-    }
-    (minutesDisplay.innerText = minutes) &&
-      (secondsDisplay.innerText = seconds);
-  }, 1000);
-
-  pauseButton.classList.add("button");
-  pauseButton.innerText = "Pause";
-
-  pauseButton.addEventListener("click", () => {
-    isPaused = !isPaused;
-    if (isPaused) {
-      pauseButton.innerText = "Resume";
-      clearInterval(timer);
-    } else {
-      pauseButton.innerText = "Pause";
-      timer = setInterval(() => {
-        if (seconds === 0) {
-          minutes -= 1;
-          seconds = 60;
-        }
-        seconds -= 1;
-        if (minutes === 0 && seconds === 0) {
-          clearInterval(timer);
-        }
-        (minutesDisplay.innerText = minutes) &&
-          (secondsDisplay.innerText = seconds);
-      }, 1000);
-    }
-  });
-};
-
-const timerOn = (minutes, seconds, minutesDisplay, secondsDisplay) => {};
